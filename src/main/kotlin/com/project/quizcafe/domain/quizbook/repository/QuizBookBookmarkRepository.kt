f2397d83f5f0764c1231f4493f3243a90c9cd91b
ErroredPasta <83020521+ErroredPasta@users.noreply.github.com>
2025-06-12 16:00:41 +0900

    refactor: QuizBookMarkValidator 삭제

--- src/main/kotlin/com/project/quizcafe/domain/quizbook/repository/QuizBookBookmarkRepository.kt
+++ src/main/kotlin/com/project/quizcafe/domain/quizbook/repository/QuizBookBookmarkRepository.kt
@@ -1,5 +1,6 @@
 package com.project.quizcafe.domain.quizbook.repository
 
+import com.project.quizcafe.common.exception.NotFoundException
 import com.project.quizcafe.domain.quizbook.entity.QuizBookBookmark
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
@@ -10,5 +11,10 @@ interface QuizBookBookmarkRepository : JpaRepository<QuizBookBookmark, Long> {
     fun findAllByUserId(userId: Long): List<QuizBookBookmark>
     fun deleteByUserIdAndQuizBookId(userId: Long, quizBookId: Long)
     fun findAllByQuizBookId(quizBookId: Long): List<QuizBookBookmark>
-
+    fun existsByUserIdAndQuizBookId(userId: Long, quizBookId: Long): Boolean
 }
+
+fun QuizBookBookmarkRepository.getQuizBookBookmarkByUserIdAndQuizBookId(
+    userId: Long,
+    quizBookId: Long
+): QuizBookBookmark = findByUserIdAndQuizBookId(userId, quizBookId) ?: throw NotFoundException("북마크가 존재하지 않습니다.")
--- src/main/kotlin/com/project/quizcafe/domain/quizbook/service/QuizBookBookmarkService.kt
+++ src/main/kotlin/com/project/quizcafe/domain/quizbook/service/QuizBookBookmarkService.kt
@@ -1,11 +1,11 @@
 package com.project.quizcafe.domain.quizbook.service
 
+import com.project.quizcafe.common.exception.ConflictException
 import com.project.quizcafe.domain.quizbook.entity.QuizBookBookmark
 import com.project.quizcafe.domain.quizbook.repository.QuizBookBookmarkRepository
 import com.project.quizcafe.domain.quizbook.repository.QuizBookRepository
+import com.project.quizcafe.domain.quizbook.repository.getQuizBookBookmarkByUserIdAndQuizBookId
 import com.project.quizcafe.domain.quizbook.repository.getQuizBookById
-import com.project.quizcafe.domain.quizbook.validator.QuizBookMarkValidator
-import com.project.quizcafe.domain.quizbook.validator.QuizBookValidator
 import com.project.quizcafe.domain.user.entity.User
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -13,13 +13,15 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class QuizBookBookmarkService(
     private val quizBookBookmarkRepository: QuizBookBookmarkRepository,
-    private val quizBookMarkValidator: QuizBookMarkValidator,
     private val quizBookRepository: QuizBookRepository
 ) {
 
     @Transactional
     fun addBookmark(user: User, quizBookId: Long) {
-        quizBookMarkValidator.validateQuizBookMarkExist(user, quizBookId)
+        if (quizBookBookmarkRepository.existsByUserIdAndQuizBookId(user.id, quizBookId)) {
+            throw ConflictException("이미 북마크한 퀴즈북입니다.")
+        }
+
         val quizBook = quizBookRepository.getQuizBookById(quizBookId)
 
         val bookmark = QuizBookBookmark(user = user, quizBook = quizBook)
@@ -28,7 +30,7 @@ class QuizBookBookmarkService(
 
     @Transactional
     fun removeBookmark(userId: Long, quizBookId: Long) {
-        val existingBookmark = quizBookMarkValidator.validateQuizBookMarkNotExist(userId, quizBookId)
+        val existingBookmark = quizBookBookmarkRepository.getQuizBookBookmarkByUserIdAndQuizBookId(userId, quizBookId)
         quizBookBookmarkRepository.delete(existingBookmark)
     }
 
