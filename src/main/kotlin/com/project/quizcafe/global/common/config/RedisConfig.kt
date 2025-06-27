1eda61b1598d7a5c98346272f5601b53a9ae2993
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2025-06-27 11:19:23 +0900

    fix : redisconfig 수정

--- src/main/kotlin/com/project/quizcafe/global/common/config/RedisConfig.kt
+++ src/main/kotlin/com/project/quizcafe/global/common/config/RedisConfig.kt
@@ -1,16 +1,21 @@
 package com.project.quizcafe.global.common.config
 
-import org.springframework.context.annotation.*
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
-class RedisConfig {
+class RedisConfig(
+    @Value("\${spring.redis.host}") private val redisHost: String,
+    @Value("\${spring.redis.port}") private val redisPort: Int,
+) {
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory =
-        LettuceConnectionFactory("localhost", 6379)
+        LettuceConnectionFactory(redisHost, redisPort)
 
     @Bean
     fun redisTemplate(): RedisTemplate<String, String> {
